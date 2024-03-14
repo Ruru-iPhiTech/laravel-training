b1a07dba4cc808298105fa41b14e42d28da64d14
Wen3131 <deguzmanaustin3131@gmail.com>
2024-03-14 11:11:35 +0800

    pre-commit with bugs
    pre-commit with bugs

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\LocaleController;
-use App\Domains\Auth\Http\Controllers\Backend\Role\RoleController;
-use App\Domains\Auth\Http\Controllers\Backend\User\UserController;
 
 /*
  * Global Routes
@@ -16,8 +14,8 @@ Route::get('lang/{lang}', [LocaleController::class, 'change'])->name('locale.cha
 /*
  * Frontend Routes
  */
-Route::group(['as' => 'frontend.'], function () {
-    includeRouteFiles(__DIR__ . '/frontend/');
+Route::group(['as' => 'admin.'], function () {
+    includeRouteFiles(__DIR__.'/frontend/');
 });
 
 /*
@@ -26,33 +24,5 @@ Route::group(['as' => 'frontend.'], function () {
  * These routes can only be accessed by users with type `admin`
  */
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'admin'], function () {
-    includeRouteFiles(__DIR__ . '/backend/');
+    includeRouteFiles(__DIR__.'/backend/');
 });
-
-Route::get('/admin/users', [UserController::class, 'index'])->name('admin.auth.user.index');
-
-Route::get('/admin/roles', [RoleController::class, 'index'])->name('admin.auth.role.index');
-
-// User roles
-Route::get('/admin/users/deactivated', [UserController::class, 'deactivated'])->name('admin.auth.user.deactivated');
-Route::get('/admin/users/deleted', [UserController::class, 'deleted'])->name('admin.auth.user.deleted');
-Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.auth.user.create');
-Route::get('/admin/users/{user}', [UserController::class, 'show'])->name('admin.auth.user.show');
-Route::get('/admin/users/{user}/edit', [UserController::class, 'edit'])->name('admin.auth.user.edit');
-Route::get('/admin/users/{user}/change-password', [UserController::class, 'changePassword'])->name('admin.auth.user.change-password');
-Route::delete('/admin/users/{user}', [UserController::class, 'destroy'])->name('admin.auth.user.destroy');
-Route::get('/admin/users/clear-session', [UserController::class, 'clearSession'])->name('admin.auth.user.clear-session');
-Route::post('/admin/users/mark', [UserController::class, 'mark'])->name('admin.auth.user.mark');
-Route::patch('/admin/users/{user}', [UserController::class, 'update'])->name('admin.auth.user.update');
-Route::post('/admin/users', [UserController::class, 'store'])->name('admin.auth.user.store');
-
-
-
-
-
-// Admin roles
-Route::get('/admin/roles/create', [RoleController::class, 'create'])->name('admin.auth.role.create');
-Route::post('/admin/roles/store', [RoleController::class, 'store'])->name('admin.auth.role.store');
-Route::get('/admin/roles/{role}/edit', [RoleController::class, 'edit'])->name('admin.auth.role.edit');
-Route::delete('/admin/roles/{role}', [RoleController::class, 'destroy'])->name('admin.auth.role.destroy');
-Route::put('/admin/roles/{role}', [RoleController::class, 'update'])->name('admin.auth.role.update');
