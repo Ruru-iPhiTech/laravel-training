4eba4c0f96a3586866fbd6693997b956395b2c76
medinaportia20 <137918731+medinaportia20@users.noreply.github.com>
2024-03-14 14:06:44 +0800

    Update 2021_04_05_153840_create_two_factor_authentications_table.php
    Name: Portia Medina
    University: University of the Assumption
    Title: Fixed bugs in Laravel admin app
    Description: This PR fixes the bugs in the Laravel admin app that affect its functionality and performance. The bugs are:
    
    - Replace timestampTz data type to timestamp
    - Replace json data type to text

--- database/migrations/2021_04_05_153840_create_two_factor_authentications_table.php
+++ database/migrations/2021_04_05_153840_create_two_factor_authentications_table.php
@@ -17,15 +17,15 @@ class CreateTwoFactorAuthenticationsTable extends Migration
             $table->bigIncrements('id');
             $table->morphs('authenticatable', '2fa_auth_type_auth_id_index');
             $table->string('shared_secret')->nullable();
-            $table->timestampTz('enabled_at')->default(now());
+            $table->timestamp('enabled_at')->useCurrent();
             $table->string('label')->unique();
             $table->unsignedTinyInteger('digits')->default(6);
             $table->unsignedTinyInteger('seconds')->default(30);
             $table->unsignedTinyInteger('window')->default(0);
             $table->string('algorithm', 16)->default('sha1');
-            $table->json('recovery_codes')->nullable();
-            $table->timestampTz('recovery_codes_generated_at')->nullable();
-            $table->json('safe_devices')->nullable();
+            $table->text('recovery_codes')->nullable();
+            $table->timestamp('recovery_codes_generated_at')->nullable();
+            $table->text('safe_devices')->nullable();
             $table->timestampsTz();
         });
     }
