693bbb684ea81688c1e934ae0b8499423f7c353f
medinaportia20 <137918731+medinaportia20@users.noreply.github.com>
2024-03-15 16:23:16 +0800

    Update web.php
    Name: Portia Medina
    University: University of the Assumption
    Title: Fixed bugs in Laravel admin app
    Description: This PR fixes the bugs in the Laravel admin app that affect its functionality and performance. The bugs are:
    
    - Added Route import
    - Replace admin to frontend
    - Add user roles routes
    - add admin roles routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LocaleController;
+use App\Domains\Auth\Http\Controllers\Backend\Role\RoleController;
+use App\Domains\Auth\Http\Controllers\Backend\User\UserController;
 
 /*
  * Global Routes
@@ -27,3 +28,33 @@ Route::group(['as' => 'frontend.'], function () {
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'admin'], function () {
     includeRouteFiles(__DIR__.'/backend/');
 });
+
+Route::prefix('admin')->group(function () {
+    // User roles
+    Route::prefix('/users')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('admin.auth.user.index');
+        Route::get('/deactivated', [UserController::class, 'deactivated'])->name('admin.auth.user.deactivated');
+        Route::get('/deleted', [UserController::class, 'deleted'])->name('admin.auth.user.deleted');
+        Route::get('/create', [UserController::class, 'create'])->name('admin.auth.user.create');
+        Route::get('/{user}', [UserController::class, 'show'])->name('admin.auth.user.show');
+        Route::get('/{user}/edit', [UserController::class, 'edit'])->name('admin.auth.user.edit');
+        Route::get('/{user}/change-password', [UserController::class, 'changePassword'])->name('admin.auth.user.change-password');
+        Route::delete('/{user}', [UserController::class, 'destroy'])->name('admin.auth.user.destroy');
+        Route::get('/clear-session', [UserController::class, 'clearSession'])->name('admin.auth.user.clear-session');
+        Route::post('/mark', [UserController::class, 'mark'])->name('admin.auth.user.mark');
+        Route::patch('/{user}', [UserController::class, 'update'])->name('admin.auth.user.update');
+        Route::post('/', [UserController::class, 'store'])->name('admin.auth.user.store');
+    });
+
+    // Admin roles
+    Route::prefix('/roles')->group(function () {
+        Route::get('/', [RoleController::class, 'index'])->name('admin.auth.role.index');
+        Route::put('/{role}', [RoleController::class, 'roleUpdate'])->name('admin.auth.role.update');
+        Route::get('/create', [RoleController::class, 'create'])->name('admin.auth.role.create');
+        Route::post('/store', [RoleController::class, 'store'])->name('admin.auth.role.store');
+        Route::get('/{role}/edit', [RoleController::class, 'edit'])->name('admin.auth.role.edit');
+        Route::delete('/{role}', [RoleController::class, 'destroy'])->name('admin.auth.role.destroy');
+        Route::put('/{role}', [RoleController::class, 'update'])->name('admin.auth.role.update');
+    });
+});
+
