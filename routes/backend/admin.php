3e6c2f8e39688e6cc9bd9afbce5c73a1aa976e79
medinaportia20 <137918731+medinaportia20@users.noreply.github.com>
2024-03-17 21:48:09 +0800

    Update admin.php
    Name: Portia Medina
    University: University of the Assumption
    Title: Fixed bugs in Laravel admin app
    Description: This PR fixes the bugs in the Laravel admin app that affect its functionality and performance. The bugs are:
    
    - Added Route import
    - Fixed admin route

--- routes/backend/admin.php
+++ routes/backend/admin.php
@@ -5,9 +5,11 @@ use App\Http\Controllers\Backend\DashboardController;
 use Tabuna\Breadcrumbs\Trail;
 
 // All route names are prefixed with 'admin.'.
-Route::redirect('/', '/admin/dashboard', 301);
-Route::get('dashboard', [DashboardController::class, 'index'])
-    ->name('admin.dashboard')
-    ->breadcrumbs(function (Trail $trail) {
-        $trail->push(__('Home'), route('admin.dashboard'));
-    });
+Route::prefix('admin')->group(function () {
+    Route::redirect('/', '/admin/dashboard', 301);
+    Route::get('dashboard', [DashboardController::class, 'index'])
+        ->name('admin.dashboard')
+        ->breadcrumbs(function (Trail $trail) {
+            $trail->push(__('Home'), route('admin.dashboard'));
+        });
+});
