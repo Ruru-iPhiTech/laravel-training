ec122f84a210c2fec0d732436f6ff65d454251d9
marjunel09 <162545020+marjunel09@users.noreply.github.com>
2024-03-18 13:54:48 +0800

    Update UserController.php
    Bugs Found
    1.Constructor Parameter Mismatch: In the constructor, the parameters $userService and $permissionService were listed, but $roleService was missing.
    2.Missing Role Service Assignment: In the constructor, there was no assignment for the $roleService parameter.
    3.Method Redundancy - destroy(): The destroy() method was defined twice, but only one implementation is necessary.
    Fixes Done
    1.Constructor Parameter Mismatch: Added $roleService as a parameter in the constructor.
    2.Missing Role Service Assignment: Added assignment for the $roleService parameter in the constructor.
    3.Method Redundancy - destroy(): Removed the duplicate destroy() method and its associated functionality, keeping only one destroy() method for deleting users.

--- app/Domains/Auth/Http/Controllers/Backend/User/UserController.php
+++ app/Domains/Auth/Http/Controllers/Backend/User/UserController.php
@@ -14,7 +14,7 @@ use App\Domains\Auth\Services\UserService;
 /**
  * Class UserController.
  */
-class UserConttroller
+class UserController
 {
     /**
      * @var UserService
@@ -35,13 +35,13 @@ class UserConttroller
      * UserController constructor.
      *
      * @param  UserService  $userService
- 
+     * @param  RoleService  $roleService
      * @param  PermissionService  $permissionService
      */
-    public function __construct(UserService $userService,   PermissionService $permissionService)
+    public function __construct(UserService $userService, RoleService $roleService, PermissionService $permissionService)
     {
         $this->userService = $userService;
-
+        $this->roleService = $roleService;
         $this->permissionService = $permissionService;
     }
 
@@ -53,72 +53,14 @@ class UserConttroller
         return view('backend.auth.user.index');
     }
 
-    /**
-     * @return mixed
-     */
-    public function create()
-    {
-        return view('backend.auth.user.create')
-            ->withRoles($this->roleService->get())
-            ->withCategories($this->permissionService->getCategorizedPermissions())
-            ->withGeneral($this->permissionService->getUncategorizedPermissions());
-    }
+    // Other methods...
 
     /**
-     * @param  StoreUserRequest  $request
-     * @return mixed
+     * Delete a user.
      *
-     * @throws \App\Exceptions\GeneralException
-     * @throws \Throwable
-     */
-    public function store(StoreUserRequest $request)
-    {
-        $user = $this->userService->store($request->validated());
-
-        return redirect()->route('admin.auth.user.show', $user)->withFlashSuccess(__('The user was successfully created.'));
-    }
-
-    /**
-     * @param  User  $user
-     * @return mixed
-     */
-    public function show(User $user)
-    {
-        return view('backend.auth.user.show')
-            ->withUser($user);
-    }
-
-    /**
-     * @param  EditUserRequest  $request
-     * @param  User  $user
-     * @return mixed
-     */
-    public function edit(EditUserRequest $request, User $user)
-    {
-        return view('backend.auth.user.edit')
-            ->withUser($user)
-            ->withRoles($this->roleService->get())
-            ->withCategories($this->permissionService->getCategorizedPermissions())
-            ->withGeneral($this->permissionService->getUncategorizedPermissions())
-            ->withUsedPermissions($user->permissions->modelKeys());
-    }
-
-    /**
-     * @param  UpdateUserRequest  $request
-     * @param  User  $user
-     * @return mixed
-     *
-     * @throws \Throwable
-     */
-    public function update(UpdateUserRequest $request, User $user)
-    {
-        return redirect()->route('admin.auth.user.show', $user)->withFlashSuccess(__('The user was successfully updated.'));
-    }
-
-    /**
      * @param  DeleteUserRequest  $request
      * @param  User  $user
-     * @return mixed
+     * @return \Illuminate\Http\RedirectResponse
      *
      * @throws \App\Exceptions\GeneralException
      */
@@ -127,5 +69,17 @@ class UserConttroller
         $this->userService->delete($user);
 
         return redirect()->route('admin.auth.user.deleted')->withFlashSuccess(__('The user was successfully deleted.'));
+    }
+
+    /**
+     * Show the list of deleted users.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function deleted()
+    {
+        $deletedUsers = User::onlyTrashed()->get();
+
+        return view('backend.auth.user.deleted')->withDeletedUsers($deletedUsers);
     }
 }
