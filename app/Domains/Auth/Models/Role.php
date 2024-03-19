5c3909bb0ce39c24178770330312bdfea24e3753
NarukamiXV <apcmendiola.student@ua.edu.ph>
2024-03-19 13:31:02 +0800

    Update Role.php
    Bugs:
    1. Missing use imports
    2. Encountering error on guard already exists
    3. $fillable property not defined
    
    Changes:
    1. Added use imports use Illuminate\Database\Eloquent\Factories\Factory
    use App\Domains\Auth\Models\Guard
    use Illuminate\Support\Facades\Auth
    use Illuminate\Support\Facades\Log
    2.Added a code for the guard already exists
    3. Added the missing $fillable property attributes

--- app/Domains/Auth/Models/Role.php
+++ app/Domains/Auth/Models/Role.php
@@ -8,22 +8,51 @@ use App\Domains\Auth\Models\Traits\Scope\RoleScope;
 use Database\Factories\RoleFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\Permission\Models\Role as SpatieRole;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Domains\Auth\Models\Guard;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 /**
  * Class Role.
  */
 class Role extends SpatieRole
 {
+    public static function create(array $attributes = [])
+{
+    $attributes['guard_name'] = $attributes['guard_name'] ?? Auth::getDefaultDriver();
+
+
+    if (static::where('name', $attributes['name'])->where('guard_name', $attributes['guard_name'])->exists()) {
+        // Log a message or handle it differently
+        Log::info("Role '{$attributes['name']}' already exists for guard '{$attributes['guard_name']}'");
+        
+        // Return null to indicate that the role already exists
+        return null;
+    }
+
+    return static::query()->create($attributes);
+}
+
     use HasFactory,
         RoleAttribute,
         RoleMethod,
         RoleScope;
 
     /**
-     * @var string[]
+     * 
+     * @var array
      */
-  
+    protected $fillable = [
+        'name',
+        'guard_name',
+        'description',
+    ];
 
+    /**
+     * @var string[]
+     *
+     */
     /**
      * Create a new factory instance for the model.
      *
