3e107793b0f66cd9911f57ce0d5b495185f31e6c
NarukamiXV <apcmendiola.student@ua.edu.ph>
2024-03-19 13:40:56 +0800

    Update web.php
    Bugs:
    1. Missing use imports on UserController, RoleController, TwoFactorAuthenticationController and DashboardController.
    2. Route::group as is set to admin
    3. Missing routes for frontend and backend
    Changes:
    1. Added the use imports for UserController, RoleController, TwoFactorAuthenticationController and DashboardController.
    2. Change the Route::group as admin. to Route::group as frontend.
    3. Added the routes for Frontend and Backend
    4. Change the route of the dashboard to /admin/dashboard and the name dashboard to admin.dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use App\Http\Controllers\LocaleController;
-
+use App\Domains\Auth\Http\Controllers\Backend\User\UserController;
+use App\Domains\Auth\Http\Controllers\Backend\Role\RoleController;
+use App\Domains\Auth\Http\Controllers\Frontend\Auth\TwoFactorAuthenticationController;
+use App\Http\Controllers\Backend\DashboardController;
+use Lab404\Impersonate\Controllers\ImpersonateController;
+use Lab404\Impersonate\Impersonate;
+use Illuminate\Support\Facades\Route;
+use Arcanedev\LogViewer\Http\Controllers\LogViewerController;
+use App\Http\Controllers\Frontend\User\AccountController;
+use App\Http\Controllers\Frontend\User\ProfileController;
+use App\Http\Controllers\Frontend\User\DashboardController as FrontendDashboardController;
+use App\Domains\Auth\Http\Controllers\Backend\User\DeletedUserController;
 /*
  * Global Routes
  *
@@ -14,15 +25,53 @@ Route::get('lang/{lang}', [LocaleController::class, 'change'])->name('locale.cha
 /*
  * Frontend Routes
  */
-Route::group(['as' => 'admin.'], function () {
+Route::group(['prefix' => 'log-viewer', 'as' => 'log-viewer::', 'middleware' => ['web', 'can:admin.access.log-viewer']], function () {
+    Route::get('/dashboard', [LogViewerController::class, 'index'])->name('dashboard');
+    Route::get('/logs', [LogViewerController::class, 'listLogs'])->name('logs.list');
+    Route::get('/logs/{date}', [LogViewerController::class, 'show'])->name('logs.show');
+    Route::get('/logs/{date}/download', [LogViewerController::class, 'download'])->name('logs.download');
+    Route::delete('/logs/delete', [LogViewerController::class, 'delete'])->name('logs.delete');
+});
+Route::group(['as' => 'frontend.'], function () {
     includeRouteFiles(__DIR__.'/frontend/');
 });
+Route::get('/account/2fa/create', [TwoFactorAuthenticationController::class, 'create'])
+    ->name('frontend.auth.account.2fa.create');
+Route::patch('/user/profile/update', [ProfileController::class, 'update'])->name('frontend.user.profile.update');
+Route::post('/auth/account/2fa/validate-code', [TwoFactorAuthenticationController::class, 'validateCode'])
+    ->name('frontend.auth.account.2fa.validateCode');
+    Route::get('/user/dashboard', [FrontendDashboardController::class, 'index'])->name('frontend.user.dashboard');
+    Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    Route::get('/users',[UserController::class, 'index'])->name('admin.auth.user.index');
+    Route::get('/roles',[RoleController::class, 'index'])->name('admin.auth.role.index');
+    Route::get('/users/deactivated', [UserController::class, 'deactivated'])->name('admin.auth.user.deactivated');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('admin.auth.user.show');
+    Route::get('admin/auth/user/deleted', 'App\Domains\Auth\Http\Controllers\Backend\User\UserController@deletedusers')->name('admin.auth.user.deleted');
+    Route::get('/users/create', [UserController::class, 'create'])->name('admin.auth.user.create');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('admin.auth.user.edit');
+    Route::get('/users/{user}/change-password', [UserController::class, 'changePassword'])->name('admin.auth.user.change-password');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('admin.auth.user.destroy');
+    Route::get('/users/clear-session', [UserController::class, 'clearSession'])->name('admin.auth.user.clear-session');
+    Route::post('/users/mark', [UserController::class, 'mark'])->name('admin.auth.user.mark');
+    Route::patch('/users/{user}', [UserController::class, 'update'])->name('admin.auth.user.update');
+    Route::post('/users', [UserController::class, 'store'])->name('admin.auth.user.store');
+    Route::post('/users/{user}/impersonate', [ImpersonateController::class, 'take'])->name('admin.auth.user.impersonate');
+    Route::get('impersonate/{user}', [ImpersonateController::class, 'take'])->name('impersonate');
+    Route::get('impersonate/leave', [ImpersonateController::class, 'leave'])->name('impersonate.leave');
 
-/*
- * Backend Routes
- *
- * These routes can only be accessed by users with type `admin`
- */
-Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'admin'], function () {
-    includeRouteFiles(__DIR__.'/backend/');
+    // Admin roles
+    Route::put('/admin/roles/{role}', [RoleController::class, 'roleUpdate'])->name('admin.auth.role.update');
+
+    Route::get('/admin/roles/create', [RoleController::class, 'create'])->name('admin.auth.role.create');
+    Route::post('/admin/roles/store', [RoleController::class, 'store'])->name('admin.auth.role.store');
+    Route::get('/admin/roles/{role}/edit', [RoleController::class, 'edit'])->name('admin.auth.role.edit');
+    Route::delete('/admin/roles/{role}', [RoleController::class, 'destroy'])->name('admin.auth.role.destroy');
+    Route::patch('/admin/roles/{role}', [RoleController::class, 'update'])->name('admin.auth.role.update');
+    /*
+    * Backend Routes
+    *
+    * These routes can only be accessed by users with type `admin`
+    */
+    Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'admin'], function () {
+        includeRouteFiles(__DIR__ . '/backend/');
 });
